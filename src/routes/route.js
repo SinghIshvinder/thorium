1e6294de7426933875d19ee5ca18dd9a49f26c17
SinghIshvinder <ishvinder.singh@science.christuniversity.in>
2022-02-25 19:31:15 +0530

    formatted

--- src/routes/route.js
+++ src/routes/route.js
@@ -3,48 +3,6 @@ const router = express.Router();
 const userModel = require('../models/userModel');
 const userController = require('../controllers/controller')
 
-
-// let persons = [
-//     {
-//     name: "PK",
-//     age: 10,
-//     votingStatus: false
-//  },
-//  {
-//     name: "SK",
-//     age: 20,
-//     votingStatus: false
-//  },
-//  {
-//     name: "AA",
-//     age: 70,
-//     votingStatus: false
-//  },
-//  {
-//     name: "SC",
-//     age: 5,
-//     votingStatus: false
-//  },
-//  {
-//     name: "HO",
-//     age: 40,
-//     votingStatus: false
-//  }
-//  ]
- 
-// router.post("/voting", function (req, res) {
-    
-//     let votingAge = req.query.votingAge;
-//     let finalArr= persons.filter( person => person.age >votingAge);
-//     for(let i=0; i<finalArr.length; i++){
-//         finalArr[i].votingStatus = true;
-//     }
-    
-
-//     console.log(finalArr);
-//     res.send({ result: finalArr , status: true })
-// })
-
 router.post('/createBook', userController.createBook);
 
 router.get('/getBook', userController.getBook );
