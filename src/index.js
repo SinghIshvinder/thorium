0bd6b6fc9d57f362f0601e291bf2e8ea00b91df7
SinghIshvinder <ishvinder.singh@science.christuniversity.in>
2022-02-25 19:28:00 +0530

    book inventory management

--- src/index.js
+++ src/index.js
@@ -2,7 +2,10 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const route = require('./routes/route.js');
 const app = express();
-
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://0.0.0.0:27017/BookAssignment' , 
+{useNewUrlParser: true, useUnifiedTopology:true}).then(() => console.log("connection successful"))
+.catch((err) => console.log('eror', err));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/', route);
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,82 +1,53 @@
 const express = require('express');
 const router = express.Router();
-
-let persons = [
-    {
-    name: "PK",
-    age: 10,
-    votingStatus: false
- },
- {
-    name: "SK",
-    age: 20,
-    votingStatus: false
- },
- {
-    name: "AA",
-    age: 70,
-    votingStatus: false
- },
- {
-    name: "SC",
-    age: 5,
-    votingStatus: false
- },
- {
-    name: "HO",
-    age: 40,
-    votingStatus: false
- }
- ]
- 
-router.post("/voting", function (req, res) {
-    
-    let votingAge = req.query.votingAge;
-    let finalArr= persons.filter( person => person.age >votingAge);
-    for(let i=0; i<finalArr.length; i++){
-        finalArr[i].votingStatus = true;
-    }
-    
-
-    console.log(finalArr);
-    res.send({ result: finalArr , status: true })
-})
+const userModel = require('../models/userModel');
+const userController = require('../controllers/controller')
 
 
-// ASSIGNMENT:
-// you will be given an array of persons ( i.e an array of objects )..each person will have  a {name: String , age: Number, votingStatus: true/false(Boolean)}
-// take input in query param as votingAge..and for all the people above that age, change votingStatus as true
-// also return an array consisting of only the person that can vote
-
-//  take this as sample for array of persons:
-// let persons= [
+// let persons = [
 //     {
 //     name: "PK",
 //     age: 10,
 //     votingStatus: false
-// },
-// {
+//  },
+//  {
 //     name: "SK",
 //     age: 20,
 //     votingStatus: false
-// },
-// {
+//  },
+//  {
 //     name: "AA",
 //     age: 70,
 //     votingStatus: false
-// },
-// {
+//  },
+//  {
 //     name: "SC",
 //     age: 5,
 //     votingStatus: false
-// },
-// {
+//  },
+//  {
 //     name: "HO",
 //     age: 40,
 //     votingStatus: false
-// }
-// ]
+//  }
+//  ]
+ 
+// router.post("/voting", function (req, res) {
+    
+//     let votingAge = req.query.votingAge;
+//     let finalArr= persons.filter( person => person.age >votingAge);
+//     for(let i=0; i<finalArr.length; i++){
+//         finalArr[i].votingStatus = true;
+//     }
+    
 
+//     console.log(finalArr);
+//     res.send({ result: finalArr , status: true })
+// })
+
+router.post('/createBook', userController.createBook);
+
+router.get('/getBook', userController.getBook );
 
 
 
